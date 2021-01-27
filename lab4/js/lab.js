d62c023cec9be9c195086331721a5102810f1e52
emlepe <emlepe@ucsc.edu>
2021-01-26 19:57:39 -0800

    lab4

---
+++ lab4/js/lab.js
@@ -0,0 +1,9 @@
+//Create a border with limitations
+//Establish point of interaction for the player
+  //Create paddle to hit the ball
+//Generate a ball for interaction
+    //Establish the ball’s potential trajectory
+      //Randomly generate inputs for ball’s trajectory
+//Establish point system for when the player scores
+//Create a target for scoring points
+//Accept player input
