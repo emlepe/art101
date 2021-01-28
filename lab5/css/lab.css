baac5ce98f28cd14b6cea767bda002df020fe7eb
emlepe <emlepe@ucsc.edu>
2021-01-28 00:57:58 -0800

    lab5

---
+++ lab5/css/lab.css
@@ -0,0 +1,21 @@
+/*
+ * Author:  Ethan Lepe <emlepe@ucsc.edu>
+ * Created:  28 January
+ * License:  Public Domain
+ */
+
+ // Define Variables
+ make = "Ford";
+ model = "Taurus";
+ color = "Rusty";
+ year = 1995;
+
+ // calculate
+ age = 2019 - year;
+
+ // output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
