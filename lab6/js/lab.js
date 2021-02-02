01b40938178eb063404764d808fb44f1a6abd32a
emlepe <emlepe@ucsc.edu>
2021-02-01 21:23:11 -0800

    lab6

---
+++ lab6/js/lab.js
@@ -0,0 +1,26 @@
+/*
+ * Author:  Ethan Lepe <emlepe@ucsc.edu>
+ * Created:  1 February
+ * License:  Public Domain
+ */
+
+// Define Variables
+
+ mytransport = ["Ford Taurus", "bike", "skateboard", "legs & feet"];
+
+ myMainRide = {
+    make = "Ford";
+    model = "Taurus";
+    color = "Rusty";
+    year = 1995;
+    age : function() {
+        return 2019 - age:
+    }
+}
+
+
+ // output
+document.writeln("Kinds of transportation I use:", myTransport, "</br>")
+document.writeln("My Main Ride: <pre>",
+    JSON.stringify(myMainRide, null, '\t'), "</pre>");
+    
