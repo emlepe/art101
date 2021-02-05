710485a8e835e559255f74c188997a08c25148a7
emlepe <emlepe@ucsc.edu>
2021-02-04 16:32:45 -0800

    font change

--- lab7/js/lab7.js
+++ lab7/js/lab7.js
@@ -10,12 +10,12 @@ function sortUserName() {
     var userName = window.prompt("Hi. Please tell me your name so I can fix it.");
     console.log("userName =", userName);
     // split string to array
-    var nameArraySort = userName.split('');
+    var nameArray = userName.split('');
     console.log("nameArray =", nameArray);
     // sort the array
     var nameArraySort = nameArray.sort();
     console.log("nameArraySort =", nameArraySort);
-    // join array back to a string
+
     var nameSorted = nameArraySort.join('');
     console.log("nameSorted =", nameSorted);
     // Note that I could have done the above lines as a single line:
@@ -26,4 +26,3 @@ function sortUserName() {
 // Output
 document.writeln("Oh hey, I've fixed your name: ",
   sortUserName(), "</br>");
-  
