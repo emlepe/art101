06b8ed03e8012a462b408c34f7ff9c3d9a75f79c
emlepe <emlepe@ucsc.edu>
2021-02-04 16:07:45 -0800

    lab 7

---
+++ lab7/js/lab7.js
@@ -0,0 +1,29 @@
+/*
+ * Author:  Ethan Lepe <emlepe@ucsc.edu>
+ * Created:  4 February
+ * License:  Public Domain
+ */
+
+// sortUserName - a function that takes user input and sorts the letters
+// of their name
+function sortUserName() {
+    var userName = window.prompt("Hi. Please tell me your name so I can fix it.");
+    console.log("userName =", userName);
+    // split string to array
+    var nameArraySort = userName.split('');
+    console.log("nameArray =", nameArray);
+    // sort the array
+    var nameArraySort = nameArray.sort();
+    console.log("nameArraySort =", nameArraySort);
+    // join array back to a string
+    var nameSorted = nameArraySort.join('');
+    console.log("nameSorted =", nameSorted);
+    // Note that I could have done the above lines as a single line:
+    // userName.toLower().split("").sort().join("")
+    return nameSorted;
+}
+
+// Output
+document.writeln("Oh hey, I've fixed your name: ",
+  sortUserName(), "</br>");
+  
