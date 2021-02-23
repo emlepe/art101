56bb0376baf50d02242878c98487d3cb69503d4c
emlepe <emlepe@ucsc.edu>
2021-02-22 21:12:47 -0800

    lab12

--- lab11/js/lab11.js
+++ lab11/js/lab11.js
@@ -5,17 +5,33 @@
  */
  // Libraries & jquery
 
- // add event handler for red button
-$("button.red").click(function(){
-  // remove possible existing class
-  $("#target").removeClass("green");
-  // now toggle red class
-  $("#target").toggleClass("red")
-});
-// add event handler for green button
-$("button.green").click(function(){
-  // remove possible existing class
-  $("#target").removeClass("red");
-  // now toggle red class
-  $("#target").toggleClass("green");
-});
+
+//     Find the section you want to add the button to
+//     Create the button
+//     Give it a text label
+//     Add it to the section
+$("#challenge").append("<button id='c-button' >Button</button>");
+$("#problem").append("<button id='p-button' >Button</button>");
+$("#results").append("<button id='r-button' >Button</button>");
+
+//    Use the JavaScript console to test all the parts of your program and see if anything went wrong
+//    Experiment with new commands in the JavaScript console to see how they work
+//    Add comments to your function to say what it does
+//    Make it neat using lots of whitespace.
+//    Style in programming matters, so use indentations and spaces where necessary.
+//    Remember to put a semicolon at the end of every line.
+$("#c-button").click(function(){
+  //find the section, toggle a class
+  // $(#challenge).toggleClass("special");
+  $(this).parent().toggleClass("special");
+  })
+$("#p-button").click(function(){
+  //find the section, toggle a class
+  // $(#challenge).toggleClass("special");
+  $(this).parent().toggleClass("special");
+  })
+$("#r-button").click(function(){
+    //find the section, toggle a class
+    // $(#challenge).toggleClass("special");
+  $(this).parent().toggleClass("special");
+  })
